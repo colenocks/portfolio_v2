5342be203d30f3b8a8cc271deb3ac761d3aa0bed
colenocks <enockscoleman@gmail.com>
2024-06-05 01:38:43 +0100

    update home content and about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,43 +4,58 @@ import Image from "next/image"
 import AnimatedText from "@/components/AnimatedText"
 import BaseLayout from "@/components/BaseLayout"
 import aboutMe from "/public/about-me.png"
-import WorkHistory from "@/components/WorkHistory"
+import { BrandXIcon, BrandGithubIcon, BrandLinkedinIcon } from "@/components/Icons"
+import Link from "next/link"
 
 export const metadata: Metadata = {
   title: "About",
 }
 
 const about = () => {
-  const paragraph1 = `I'm Coleman Enocks, a software developer with 3+ years of experience in the field which along with my educational background has endowed me with strong problem solving, analytical and attention to details skills to model and develop standards-compliant websites and applications using modern technologies.`
-
-  const paragraph2 = `I work brilliantly in a team setup and there's no doubt that I bring committment and determination to ensure the excellence of the project or product. I look forward to the opportunity to working with you on your next project.`
-
-  const paragraph3 = `When I'm not working, I play football on a semi-professional level - an exciting way to achieve the necessary balance. I am your friendly neighbourhood Programmer by Day and Footballer by night.`
+  const paragraphs = [
+    { id: "1", text: "I'm Coleman Enocks, a Full Stack developer with 3+ years of experience in the field. I specialize in frontend, primarily Vue (and React), but love building with whatever tools are right for the job." },
+    // {id: "2", text: `I work brilliantly in a team setup and there's no doubt that I bring committment and determination to ensure the excellence of the project or product. I look forward to the opportunity to working with you on your next project.`},
+    { id: "2", text: "My experience & educational background has endowed me with strong problem solving, analytical and attention to details skills to model and develop standards-compliant websites and applications using modern technologies." },
+    { id: "3", text: `When I'm not working, I play football on a semi-professional level - an exciting way to achieve the necessary balance. I am your friendly neighbourhood Programmer by Day and Footballer by night.` },
+    { id: "4", text: "I'm actively looking for new positions where I can translate my love for code into beautiful applications. If you think you've got an opening suitable for me, you can reach out for a chat or coffee 🤗" },
+  ]
 
   return (
     <BaseLayout className={"space-y-20"}>
       <div>
-        <AnimatedText text="Drive, Dedication, Grace" />
-        <div className={"flex items-center"}>
+        <AnimatedText text="About." />
+        <div className={"flex items-center gap-12"}>
           <div className={"mt-4 w-1/2"}>
-            <h2 className={"mb-6 text-xl font-bold uppercase"}>BIO</h2>
             <div className={"flex flex-col gap-y-6"}>
-              <p className={"font-medium"}>{paragraph1}</p>
-              <p className={"font-medium"}>{paragraph2}</p>
-              <p className={"font-medium"}>{paragraph3}</p>
+              {paragraphs.map((paragraph) => {
+                return (
+                  <p className={"font-medium"} key={paragraph.id}>
+                    {paragraph.text}
+                  </p>
+                )
+              })}
+            </div>
+            <div className={"my-6 flex items-center gap-x-4 font-medium "}>
+              <div className="flex items-center gap-x-4">
+                <Link href={"https://linkedin.com/in/coleman-enocks"}>
+                  <BrandLinkedinIcon className="scale:100 cursor-pointer transition-all ease-linear hover:scale-125 hover:text-pink-400" size={36} stroke={1.5} />
+                </Link>
+                <Link href={"https://github.com/colenocks"}>
+                  <BrandGithubIcon className="scale:100 cursor-pointer transition-all ease-linear hover:scale-125 hover:text-pink-400" size={28} />
+                </Link>
+                <Link href={"https://x.com/@colenocks"}>
+                  <BrandXIcon className="scale:100 cursor-pointer transition-all ease-linear hover:scale-125 hover:text-pink-400" size={28} />
+                </Link>
+              </div>
             </div>
           </div>
 
-          <div className={"flex w-1/2 items-center justify-center"}>
-            <div className={"w-80 border-white bg-black p-8"}>
+          <div className={"flex w-1/2 items-start"}>
+            <div className={"w-64 bg-black p-8 lg:-mt-20"}>
               <Image src={aboutMe} alt="Coleman Enocks" className={"h-auto w-full rounded-full "} />
             </div>
           </div>
         </div>
-      </div>
-
-      <div>
-        <WorkHistory />
       </div>
     </BaseLayout>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,17 @@ export default function Home() {
   return (
     <BaseLayout>
       <div className="mx-auto w-1/2">
-        <AnimatedText text="Bringing Ideas To life with Code" />
+        <AnimatedText text="Hey, I'm Cole." />
         <p className="my-4 text-base font-medium text-white">
-          As a software engineer, I have been endowed with just enough expertise to turning ideas into innovative web applications. At the moment, I have quite a <span className={"underline"}>light</span> catalog of projects you can explore, but
-          there are more exciting ones in the works. 🏗️
+          As a <span className="text-pink-400">Full Stack developer</span>, I have just enough expertise to turn ideas into innovative applications. If you find this exciting and want to get in touch, let&#39;s connect!.
         </p>
 
         <div className="mt-2 flex items-center gap-4 self-start">
           <ButtonLink link="https://drive.google.com/file/d/146rG5DCqhHxyoR9-rXVntkcZbXU88WIN/view?usp=sharing" target={"_blank"} downloadable>
-            Resume <ExternalLinkIcon className={"mb-0.5 ml-1 w-5"} />
+            My Resume <ExternalLinkIcon className={"mb-0.5 ml-1 w-5"} />
           </ButtonLink>
-          <Link href="mailto:enockscoleman@gmail.com" target={"_blank"} className={"font-medium capitalize text-white hover:underline"}>
-            Contact me
+          <Link href="/contact" className={"font-medium capitalize text-white hover:underline"}>
+            Contact Me
           </Link>
         </div>
       </div>
