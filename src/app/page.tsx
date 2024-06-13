bf1a6807c0d5801b89b9915044d6215215004452
colenocks <enockscoleman@gmail.com>
2024-06-13 23:56:36 +0100

    update resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,14 @@ export default function Home() {
     <BaseLayout>
       <div className="mx-auto w-full text-center md:w-[70%] lg:text-left xl:w-[60%]">
         <div className="flex flex-col items-center ">
-          <AnimatedText text="Hey, I'm Cole." className={"text-center lg:text-left"} />
+          <AnimatedText text="Hi, I'm Cole." className={"text-center lg:text-left"} />
           <p className="my-4 mr-auto w-full text-base font-medium text-white lg:w-[90%] xl:w-[70%]">
             As a <span className="text-pink-400">Full Stack developer</span>, I have just enough expertise to turn ideas into innovative applications. If you find this exciting and want to get in touch, let&#39;s connect!
           </p>
         </div>
 
         <div className="mt-2 flex flex-col items-center justify-center gap-4 self-start lg:flex-row lg:justify-start">
-          <ButtonLink link="https://drive.google.com/file/d/146rG5DCqhHxyoR9-rXVntkcZbXU88WIN/view?usp=sharing" target={"_blank"} downloadable>
+          <ButtonLink link="https://drive.google.com/file/d/11Y15PI_DRih2bvYptZ-LXCkf_hQHVMUA/view?usp=sharing" target={"_blank"} downloadable>
             My Resume <ExternalLinkIcon className={"mb-0.5 ml-1 w-5"} />
           </ButtonLink>
           <Link href="/contact" className={"font-medium capitalize text-white hover:underline"}>
